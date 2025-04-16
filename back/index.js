f923210afe51ee10adeff23abf413251b263c568
MelakuAzene21 <melakuazene623@gmail.com>
2025-04-17 00:53:35 +0300

    fix route

--- back/index.js
+++ back/index.js
@@ -1,7 +1,13 @@
 const app = require("./app");
 const db = require('./config/db')
 const port = 3000;
-app.listen(port,()=>{
-console.log(`Server Listening on Port
-http://localhost:${port}`);
-})
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      status: false,
+      error: err.message || 'Something went wrong',
+    });
+  });
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const UserController = require('../controller/user.controller');
-router.post("/register",UserController.register);
+router.post("/registration",UserController.register);
 router.post("/login", UserController.login);
 module.exports = router;
